b52b4637330759d4134b40ad1c0d4fab3f2c4c0d
kimjiuk <kims2521@naver.com>
2025-01-27 04:26:35 +0900

    네이버 사용자 로그인 기능 추가

--- src/main/java/SpringBootExample/JavaOrg/config/auth/dto/OAuthAttributes.java
+++ src/main/java/SpringBootExample/JavaOrg/config/auth/dto/OAuthAttributes.java
@@ -24,10 +24,15 @@ public class OAuthAttributes {
         this.email = email;
         this.picture = picture;
     }
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes)
-    {
+
+    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
+
     private static OAuthAttributes ofGoogle(String userNameAttributesName, Map<String, Object> attributes)
     {
         return OAuthAttributes.builder()
@@ -36,6 +41,17 @@ public class OAuthAttributes {
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributesName)
+                .build();
+    }
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
                 .build();
     }
     public User toEntity()
