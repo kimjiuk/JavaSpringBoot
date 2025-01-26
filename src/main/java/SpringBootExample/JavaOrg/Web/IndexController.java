ed265d65a4f99a01b3590b1bc82919179711f188
kimjiuk <kims2521@naver.com>
2025-01-27 02:39:18 +0900

    스프링세션을 jdbc로 저장소 구성

--- src/main/java/SpringBootExample/JavaOrg/Web/IndexController.java
+++ src/main/java/SpringBootExample/JavaOrg/Web/IndexController.java
@@ -1,5 +1,6 @@
 package SpringBootExample.JavaOrg.Web;
 import SpringBootExample.JavaOrg.Web.dto.PostsResponseDto;
+import SpringBootExample.JavaOrg.config.auth.LoginUser;
 import SpringBootExample.JavaOrg.config.auth.dto.SessionUser;
 import SpringBootExample.JavaOrg.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +16,12 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+    //private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if( user!=null )
         {
             model.addAttribute("userName", user.getName());
